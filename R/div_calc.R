ba0a867c6627f6190255889ca5059784d531d54b
Marco <marco.girardello@gmail.com>
2023-03-20 18:03:04 +0100

    updated div_calc

--- R/div_calc.R
+++ R/div_calc.R
@@ -59,11 +59,11 @@ div_calc<-function(x){
   tmp1 = tmp
   sp::coordinates(tmp1) = ~x+y
   #tmp1 <- tibble::tibble(tmp1) %>% mutate(rh98 = rh98/10000)
-  sp::proj4string(tmp1) <- sp::CRS("EPSG:4326")
+  sp::proj4string(tmp1) <- sp::CRS("+proj=moll +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs")
   tmp2 <- sp::spTransform(tmp1, sp::CRS("+proj=moll +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs"))
   #f <- try(automap::autofitVariogram(rh98 ~ 1, tmp2))
   Vario1 <- gstat::variogram(rh98 ~ 1, tmp2, cloud= FALSE)##,cutoff = 5000)#, )
-  f <-  fit.variogram(Vario1,  vgm(c("Exp", "Mat", "Sph")), fit.kappa = TRUE)
+  f <-  gstat::fit.variogram(Vario1,  vgm(c("Exp", "Mat", "Sph")), fit.kappa = TRUE)
 
   ## Look at the result of the fit
   # f
