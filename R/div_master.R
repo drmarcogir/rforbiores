abd3bad15083f1e1a5d1af92074091f0e0aec0c2
Marco <marco.girardello@gmail.com>
2023-04-03 16:29:39 +0200

    updayte div_master script

--- R/div_master.R
+++ R/div_master.R
@@ -1,22 +1,29 @@
-#' Helper function for creating an irregular sequence of numbers
-#' @inlist = input list to be processed
-#' @cpu = number of cpus to be used
-#'
-#'
+######################################################################
+#'  Function for preparing data for diversity calculations
+#'  @ indf = path to csv file containing the data
+#'####################################################################
 
-div_master<-function(inlist,cpu){
-  # generate sequence (to loop through)
-  SeqFinal <- seqlast(from=0,to=length(inlist),by=1000)
-  # list where to store results
-  mylist <- list()
-  # loop through sequence
-  for (i in 1:(length(SeqFinal)-1)){
-    plan(multisession, workers = cpu)
-    future_lapply(inlist[(SeqFinal[i]+1):SeqFinal[i+1]],div_calc_wrap,future.seed=TRUE)->res
-    mylist <- append(mylist,res)
-    # close session (change plan)
-    plan(sequential)
+div_master <- function(data,colname){
+
+  #  Part 1 read in data
+  dat<-fread(data)
+  cat("\n still preparing the data... hang in there!")
+
+  # subset column with grid name and get unique values
+  dat %>%
+    pull(colname) %>%
+    unique() %>%
+    # split into list
+    split(ceiling(seq_along(.) / 1000)) -> vl
+
+  # split data into 10 files using the lookup list
+  for (i in 1:length(vl)){
+    tmp <- dat %>%
+      filter(!!sym(colname) %in% vl[[i]])
+    write_csv(tmp,paste0("./data/input_analyses/splitfiles/file_",i,".csv"))
   }
-  return(mylist)
+  #  Part 2 run calculations
+
 }
+
 
